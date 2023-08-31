74c9687bf246288df3e243193ac01f17963f4ae5
凃锦东 <1965382331@qq.com>
2023-08-31 16:52:02 +0800

    test: modal组件添加单元测试。

--- packages/components/src/modal/__tests__/modal.test.tsx
+++ packages/components/src/modal/__tests__/modal.test.tsx
@@ -1,13 +1,56 @@
+import { nextTick } from 'vue'
 import { mount } from '@vue/test-utils'
-import { describe, expect, test } from 'vitest'
-import Modal from '../src/modal.vue'
+import { describe, expect, test, vi } from 'vitest'
+import Modal from '../index'
+import ModalComponent from '../src/modal.vue'
 
 const AXIOM = 'Tu view is good'
 
-describe('Modal.vue', () => {
-  test('render test', () => {
-    const wrapper = mount(() => <Modal>{AXIOM}</Modal>)
+describe('Modal', () => {
+  test('should render modal and emit ok/cancel event', async () => {
+    const wrapper = mount(ModalComponent, {
+      props: {
+        defaultVisible: true,
+        renderToBody: false,
+      },
+      slots: {
+        default: `<div>${AXIOM}</div>`,
+        title: 'Title',
+      },
+    });
 
-    expect(wrapper.text()).toEqual(AXIOM)
-  })
+    await nextTick();
+    expect(wrapper.html()).toMatchSnapshot();
+    const buttons = wrapper.findAll('button');
+    await buttons[0].trigger('click');
+    expect(wrapper.emitted('cancel')).toHaveLength(1);
+    await buttons[1].trigger('click');
+    expect(wrapper.emitted('ok')).toHaveLength(1);
+  });
+
+  test('should show modal and call cb', async () => {
+    const onOk = vi.fn()
+    const onCancel = vi.fn()
+
+    const wrapper = mount({
+      template: `<button @click="handleClick">Click</button>`,
+      methods: {
+        handleClick() {
+          Modal.open({
+            title: 'title',
+            content: 'content',
+            onOk,
+            onCancel,
+          });
+        },
+      },
+    });
+
+    await wrapper.find('button').trigger('click');
+    const buttons = document.querySelectorAll('button');
+    await buttons[0]?.click();
+    expect(onCancel).toBeCalled();
+    await buttons[1]?.click();
+    expect(onOk).toBeCalled();
+  });
 })
