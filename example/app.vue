dc310068606b0f403891d97b5b5f0a277bd36abc
凃锦东 <1965382331@qq.com>
2023-09-06 18:05:16 +0800

    chore: 添加popover样式。

--- example/app.vue
+++ example/app.vue
@@ -22,6 +22,78 @@
 
     <br />
     <br />
+    <div
+      :style="{
+        position: 'relative',
+        width: '440px',
+        height: '280px',
+        marginLeft: '100px'
+      }"
+    >
+      <tu-tooltip :default-popup-visible="true" content="123" position="tl">
+        <tu-button style="position: absolute; top: 0; left: 90px">
+          上左
+        </tu-button>
+      </tu-tooltip>
+      <tu-tooltip :default-popup-visible="true" content="123" position="top">
+        <tu-button style="position: absolute; top: 0; left: 180px">
+          上方
+        </tu-button>
+      </tu-tooltip>
+      <tu-tooltip :default-popup-visible="true" content="123" position="tr">
+        <tu-button style="position: absolute; top: 0; left: 280px">
+          上右
+        </tu-button>
+      </tu-tooltip>
+      <tu-tooltip :default-popup-visible="true" content="123" position="bl">
+        <tu-button style="position: absolute; top: 240px; left: 80px">
+          下左
+        </tu-button>
+      </tu-tooltip>
+      <tu-tooltip :default-popup-visible="true" content="123" position="bottom">
+        <tu-button style="position: absolute; top: 240px; left: 180px">
+          下方
+        </tu-button>
+      </tu-tooltip>
+      <tu-tooltip :default-popup-visible="true" content="123" position="br">
+        <tu-button style="position: absolute; top: 240px; left: 280px">
+          下右
+        </tu-button>
+      </tu-tooltip>
+      <tu-tooltip :default-popup-visible="true" content="123" position="lt">
+        <tu-button style="position: absolute; top: 60px; left: 10px">
+          左上
+        </tu-button>
+      </tu-tooltip>
+      <tu-tooltip :default-popup-visible="true" content="123" position="left">
+        <tu-button style="position: absolute; top: 120px; left: 10px">
+          左方
+        </tu-button>
+      </tu-tooltip>
+      <tu-tooltip :default-popup-visible="true" content="123" position="lb">
+        <tu-button style="position: absolute; top: 180px; left: 10px">
+          左下
+        </tu-button>
+      </tu-tooltip>
+      <tu-tooltip :default-popup-visible="true" content="123" position="rt">
+        <tu-button style="position: absolute; top: 60px; left: 350px">
+          右上
+        </tu-button>
+      </tu-tooltip>
+      <tu-tooltip :default-popup-visible="true" content="123" position="right">
+        <tu-button style="position: absolute; top: 120px; left: 350px">
+          右方
+        </tu-button>
+      </tu-tooltip>
+      <tu-tooltip :default-popup-visible="true" content="123" position="rb">
+        <tu-button style="position: absolute; top: 180px; left: 350px">
+          右下
+        </tu-button>
+      </tu-tooltip>
+    </div>
+
+    <br />
+    <br />
 
     <tu-select placeholder="请输入">
       <tu-option>北京</tu-option>
